3b1468ac170af424b88d50c5dcd98e4cfc4b8863
VanLinh <phamvanlinh9b.lhp@gmail.com>
2023-02-24 21:37:27 +0700

    baitap

---
+++ baitap.py
@@ -0,0 +1,169 @@
+# bai10
+import math
+def factorial(n):
+    if(n == 1): 
+        return 1
+    else:
+        return (n * factorial(n-1))
+    
+def tichchan(n):
+    rs = 1
+    for i in range(1, n + 1) :
+        if(i%2 == 0):
+            rs = rs * i 
+    return rs
+
+def tichle(n):
+    rs = 1
+    for i in range(1, n + 1) :
+        if(i%2 != 0):
+            rs = rs * i 
+    return rs
+
+
+
+print("Nhập x: ")
+x = float(input())
+print("Nhập eps: ")
+eps = float(input())
+
+
+
+# #bai1
+mu = 1
+first = 1
+second = first + (x**mu)/factorial(mu)
+while (math.fabs(first - second) > eps):   
+    mu += 1
+    first = second
+    second = first + (x**mu)/factorial(mu)
+print(first)
+
+
+#bai2
+mu = 1
+dau = 1
+first = 1
+second = first + dau * ((((first+1)*(first+2))/2)*x**mu)
+while (math.fabs(first - second) > eps):   
+    mu += 1
+    dau = -dau
+    first = second
+    second = first + dau * ((((first+1)*(first+2))/2)*x**mu)
+print(first)
+
+# Bai3 
+mu = 1
+first = - 1
+second = first - (x**mu)/mu
+while (math.fabs(first - second) > eps):   
+    mu += 1
+    first = second
+    second = first - (x**mu)/mu
+print(first)
+
+# bai4
+mu = 1
+dau = 1
+first = 1
+second = first + dau * tichle(mu * 2 - 2)/ tichchan(mu * 2) * x**mu
+while (math.fabs(first - second) > eps):   
+    mu += 1
+    dau = -dau
+    first = second
+    second = first + dau * tichle(mu * 2 - 2)/ tichchan(mu * 2) * x**mu
+print(first)
+
+# Bai5
+mu = 1
+dau = 1
+first = -1
+second = first + dau * tichle(mu * 2 - 2)/ tichchan(mu * 2) * x**mu
+while (math.fabs(first - second) > eps):   
+    mu += 1
+    dau = -dau
+    first = second
+    second = first + dau * tichle(mu * 2 - 2)/ tichchan(mu * 2) * x**mu
+print(first)
+
+# Bai6
+mu = 1
+dau = -dau
+first = x
+second = first + dau*(x**mu)/factorial(mu)
+while (math.fabs(first - second) > eps):   
+    mu += 2
+    dau = dau
+    first = second
+    second = first + dau*(x**mu)/factorial(mu)
+print(first)
+
+# Bai7
+mu = 1
+dau = -dau
+first = 1
+second = first + dau*(x**mu)/factorial(mu)
+while (math.fabs(first - second) > eps):   
+    mu += 2
+    dau = dau
+    first = second
+    second = first + dau*(x**mu)/factorial(mu)
+print(first)
+
+# Bai8
+mu = 3
+first = x
+second = first * tichle(mu - 2)/ tichchan(mu - 1) * x**mu / mu
+while (math.fabs(first - second) > eps):   
+    mu += 2
+    first = second
+    second = first * tichle(mu - 2)/ tichchan(mu - 1) * x**mu / mu  
+print(first)
+
+# Bai9
+mu = 1
+dau = -dau
+first = 1
+second = first + dau*(x**mu)/factorial(mu+1)
+while (math.fabs(first - second) > eps):   
+    mu += 2
+    dau = dau
+    first = second
+    second = first + dau*(x**mu)/factorial(mu+1)
+print(first)
+
+
+#Bai10
+mu = 1
+dau = 1
+first = 0
+second = first + dau * x ** mu / mu
+while (math.fabs(first - second) > eps):   
+    mu += 2
+    dau = -dau
+    first = second
+    second = first + dau * math.pow(x,mu) / mu
+print( first)
+
+# Bai 11
+mu = 1
+dau = -dau
+first = x
+second = first + dau*(x**mu)/factorial(mu)
+while (math.fabs(first - second) > eps):   
+    mu += 1
+    dau = dau
+    first = second
+    second = first + dau*(x**mu)/factorial(mu)
+print(first)
+# Bai 12
+mu = 1
+first = 1
+second = 2 * (first * (mu + 2) * x**mu / mu)
+while (math.fabs(first - second) > eps):   
+    mu += 2
+    first = second
+    second = 2 * (first * (mu + 2) * x**mu / mu)
+print(first)
+
+
